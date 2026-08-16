1999459014a7f4172cb61c3f598e8969de6c1165
Master Alex <masteralex@kali>
2026-08-16 23:30:53 +0100

    Restore User-Agent constants in iOS screens and disable IPA site folder publishing

--- composeApp/src/iosMain/kotlin/com/alexleoreeves/novelapp/ui/MaServerPlayerScreen.ios.kt
+++ composeApp/src/iosMain/kotlin/com/alexleoreeves/novelapp/ui/MaServerPlayerScreen.ios.kt
@@ -258,3 +258,8 @@ private fun String.providerDisplayName(): String {
     }
 }
 
+private const val MA_EMBED_USER_AGENT =
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+        "(KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+
+
--- composeApp/src/iosMain/kotlin/com/alexleoreeves/novelapp/ui/YouTubePlayerScreen.ios.kt
+++ composeApp/src/iosMain/kotlin/com/alexleoreeves/novelapp/ui/YouTubePlayerScreen.ios.kt
@@ -255,3 +255,8 @@ private class YouTubeNavigationDelegate(
     }
 }
 
+private const val YOUTUBE_USER_AGENT =
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 " +
+        "(KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1"
+
+
